3bec4d445c92e96c23e2dcf7874df06bb272ea36
uwec <uwe650@hotmail.com>
2015-01-11 09:31:37 -0500

    changed database service

--- server/js/services/database.js
+++ server/js/services/database.js
@@ -1,44 +1,44 @@
 'user strict';
 app.service("Database", function() {
 
-
-var GameScore = Parse.Object.extend("GameScore");
-var gameScore = new GameScore();
-var query = new Parse.Query(GameScore);
+var Gigs = Parse.Object.extend("Gigs");
+var gig = new Gigs();
+var query = new Parse.Query(Gigs);
 
 this.setObject = function() {
-	gameScore.set("score", 1337);
-	gameScore.set("playerName", "Sean Plott");
-	gameScore.set("cheatMode", false);
+	gig.set("timestamp", 133702385124);
+	gig.set("user", "Uwe Cerron");
+	gig.set("currency", "USD");
+	gig.set("Description", "burrito");
+	gig.set("txHash", "this is where the hash is");
+	gig.set("Status", "Pending Delivery");
 	 
-	gameScore.save(null, {
-	  success: function(gameScore) {
+	gig.save(null, {
+	  success: function(gig) {
 	    // Execute any logic that should take place after the object is saved.
-	    alert('New object created with objectId: ' + gameScore.id);
+	    console.log('New object created with objectId: ' + gig.id);
 	  },
-	  error: function(gameScore, error) {
+	  error: function(gig, error) {
 	    // Execute any logic that should take place if the save fails.
 	    // error is a Parse.Error with an error code and message.
-	    alert('Failed to create new object, with error code: ' + error.message);
+	    console.log('Failed to create new object, with error code: ' + error.message);
 	  }
 	});
 }
 
 this.fetchObject = function() {
-
-	query.find({
-	  success: function(results) {
-	    console.log("Successfully retrieved " + results.length + " scores.");
-	    // Do something with the returned Parse.Object values
-	    for (var i = 0; i < results.length; i++) { 
-	      var object = results[i];
-	      console.log(object.id + ' - ' + object.get('playerName'));
-	    }
-	  },
-	  error: function(error) {
-	    console.log("Error: " + error.code + " " + error.message);
-	  }
-	});
+  query.find({
+  success: function(results) {
+    console.log("Successfully retrieved " + results.length + " gigs.");
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) { 
+      var object = results[i];
+      console.log(object.id + ' - ' + object.get('user'));
+    }
+  },
+  error: function(error) {
+    console.log("Error: " + error.code + " " + error.message);
+  }
 }
 
 
